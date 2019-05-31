c074ec05477a7fbe0f8f56e04ddf9973936133db
Sagi Janos <jsagi@adverticum.com>
2019-05-31 21:22:51 +0200

    Updates

---
+++ precache-manifest.adb7aa064cbd0d7a793e42116fdbd85f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3c357fa5f30b09221b5",
+    "url": "/color-app/static/css/main.bd945bc2.chunk.css"
+  },
+  {
+    "revision": "d3c357fa5f30b09221b5",
+    "url": "/color-app/static/js/main.0ff70f47.chunk.js"
+  },
+  {
+    "revision": "ea958c06051b899540ac",
+    "url": "/color-app/static/js/runtime~main.bf85e199.js"
+  },
+  {
+    "revision": "af4451ae8c35c33753ba",
+    "url": "/color-app/static/css/2.bd8db383.chunk.css"
+  },
+  {
+    "revision": "af4451ae8c35c33753ba",
+    "url": "/color-app/static/js/2.c439ba46.chunk.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/color-app/static/media/bg.77428cd4.svg"
+  },
+  {
+    "revision": "ed1eb7925af677d6b9230a264f27a303",
+    "url": "/color-app/index.html"
+  }
+];
